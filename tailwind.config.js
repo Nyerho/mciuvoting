39732533600967f85f1801a609ced7364b48af8e
Nyerho <nyerho@users.noreply.github.com>
2026-04-01 08:43:31 +0100

    Build Tailwind CSS locally (remove CDN)

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1e3a8a',
+        secondary: '#f1f5f9'
+      }
+    }
+  },
+  plugins: []
+};
